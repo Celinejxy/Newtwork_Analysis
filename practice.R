3f965c8b2b940e913153e6834cc70efc9930b962
celineWhisper <xj2272@columbia.edu>
2023-04-27 22:35:51 -0500

    uploaded working_adjacency matrix

--- practice.R
+++ practice.R
@@ -1,10 +1,16 @@
 library(igraph)
-friendship <- read.csv('friendship_edgelist.csv')
-friendship
-
+friendship <- read.csv('EdgeList_ds/friendship_edgelist.csv')
 friends <- graph.data.frame(as.matrix(friendship), directed = FALSE)
 friends2 <- simplify(friends, remove.multiple = FALSE, remove.loops = TRUE)
 friend_adjacency <- get.adjacency(friends2, sparse = FALSE)
 class(friend_adjacency)
 friend_adjacency
 write.csv(friend_adjacency,"friend_adjacency.csv")
+
+working <- read.csv('EdgeList_ds/working_edgeList.csv', header=FALSE)
+work <- graph.data.frame(as.matrix(working), directed = FALSE)
+work2 <- simplify(work, remove.multiple = FALSE, remove.loops = TRUE)
+work_adjacency <- get.adjacency(work2, sparse = FALSE)
+
+work_adjacency
+write.csv(work_adjacency,"work_adjacency.csv")
