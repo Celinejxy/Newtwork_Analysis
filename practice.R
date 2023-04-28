c9b04f9a19aa6f88c659026291a636f6b7867ee0
celineWhisper <xj2272@columbia.edu>
2023-04-27 22:18:08 -0500

    corrected loop

--- practice.R
+++ practice.R
@@ -1,7 +1,10 @@
 library(igraph)
 friendship <- read.csv('friendship_edgelist.csv')
+friendship
+
 friends <- graph.data.frame(as.matrix(friendship), directed = FALSE)
-friends
-friend_adjacency <- get.adjacency(friends, sparse = FALSE)
+friends2 <- simplify(friends, remove.multiple = FALSE, remove.loops = TRUE)
+friend_adjacency <- get.adjacency(friends2, sparse = FALSE)
 class(friend_adjacency)
+friend_adjacency
 write.csv(friend_adjacency,"friend_adjacency.csv")
