6f9bd2072f49083d4c990ddc9c9c527e8d7c9ea6
celineWhisper <xj2272@columbia.edu>
2023-04-27 21:32:08 -0500

    uploaded adjacency matrix for friendship

--- practice.R
+++ practice.R
@@ -1,14 +1,7 @@
-install.packages('igraph')
-library(igraph)
-el=matrix(c('a','b','c','d','a','d','a','b','c','d'),ncol=2,byrow=TRUE) #a sample edgelist
-g=graph.data.frame(el)
-get.adjacency(g,sparse=FALSE)
-
-print(el)
-g
-
 library(igraph)
 friendship <- read.csv('friendship_edgelist.csv')
 friends <- graph.data.frame(as.matrix(friendship), directed = FALSE)
 friends
-get.adjacency(friends, sparse = FALSE)
+friend_adjacency <- get.adjacency(friends, sparse = FALSE)
+class(friend_adjacency)
+write.csv(friend_adjacency,"friend_adjacency.csv")
